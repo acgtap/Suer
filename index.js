66da5c22b7477b5b444aee64c16bd102660fcfe6
acgtap <2480321637@qq.com>
2022-08-13 17:01:21 +0800

    update

--- index.js
+++ index.js
@@ -49,6 +49,28 @@ app.get("/api/wx_openid", async (req, res) => {
   }
 });
 
+// 消息回复
+app.get("/api/message", async (req, res) => {
+  if (req.headers["x-wx-source"]) {
+    let example = {
+      ToUserName: "gh_919b00572d95", // 小程序/公众号的原始ID，资源复用配置多个时可以区别消息是给谁的
+      FromUserName: "oVneZ57wJnV-ObtCiGv26PRrOz2g", // 该小程序/公众号的用户身份openid
+      CreateTime: 1651049934, // 消息时间
+      MsgType: "text", // 消息类型
+      Content: "回复文本", // 消息内容
+      MsgId: 23637352235060880, // 唯一消息ID，可能发送多个重复消息，需要注意用此 ID 去重
+    };
+    console.log("消息推送", req.body);
+    res.send({
+      "ToUserName": req.body.ToUserName,
+      "FromUserName": req.body.FromUserName,
+      "CreateTime": Date.now(), 
+      "MsgType": "text",
+      "Content": "你好"
+    });
+  }
+});
+
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
